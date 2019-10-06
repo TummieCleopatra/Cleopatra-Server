24c0c8c701f8033e7579d42b9edf27a37cb268af
Tummie <tummieofgaruda@gmail.com>
2019-10-06 08:28:02 -0700

    Ergon Locus Fix
    Fixed Ergon Locus

--- scripts/zones/Konschtat_Highlands/npcs/Ergon_Locus.lua
+++ scripts/zones/Konschtat_Highlands/npcs/Ergon_Locus.lua
@@ -6,12 +6,13 @@
 require("scripts/globals/status")
 require("scripts/globals/quests")
 require("scripts/globals/keyitems")
+local ID = require("scripts/zones/Konschtat_Highlands/IDs");
 -----------------------------------
 
 function onTrigger(player,npc)
     if (player:getVar("GeoQuest") == 1) then
         player:setVar("GeoQuest",2)
         player:addKeyItem(dsp.ki.FISTFUL_OF_HOMELAND_SOIL)
-        player:messageSpecial(ID.text.KEY_ITEM_OBTAINED,FISTFUL_OF_HOMELAND_SOIL)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.FISTFUL_OF_HOMELAND_SOIL)
     end
 end;
--- scripts/zones/La_Theine_Plateau/npcs/Ergon_Locus.lua
+++ scripts/zones/La_Theine_Plateau/npcs/Ergon_Locus.lua
@@ -1,17 +1,18 @@
 -----------------------------------
--- Area: Tahrongi Canyon
+-- Area: La Theine Plateau
 --  NPC: ErgonLocus
 -- !pos
 -----------------------------------
 require("scripts/globals/status")
 require("scripts/globals/quests")
 require("scripts/globals/keyitems")
+local ID = require("scripts/zones/La_Theine_Plateau/IDs");
 -----------------------------------
 
 function onTrigger(player,npc)
     if (player:getVar("GeoQuest") == 1) then
         player:setVar("GeoQuest",2)
         player:addKeyItem(dsp.ki.FISTFUL_OF_HOMELAND_SOIL)
-        player:messageSpecial(ID.text.KEY_ITEM_OBTAINED,FISTFUL_OF_HOMELAND_SOIL)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.FISTFUL_OF_HOMELAND_SOIL)
     end
 end;
