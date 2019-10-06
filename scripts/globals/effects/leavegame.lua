dee0e5a64423965ee055f6f8b4e08e61802f5a8b
Tummie <tummieofgaruda@gmail.com>
2019-10-06 08:44:20 -0700

    Resting bonus logout variable
    adding logout variable

--- scripts/globals/effects/leavegame.lua
+++ scripts/globals/effects/leavegame.lua
@@ -11,6 +11,8 @@ end
 
 function onEffectTick(target,effect)
     if (effect:getTickCount() > 5) then
+        target:setVar("logoutRestStart", os.time())
+        target:setVar("logoutOK",1)
         target:leavegame()
     else
         target:messageSystem(effect:getPower(),30-effect:getTickCount()*5)
