de6727aad8234958931c2fd0bddabf8606f08a8c
Tummie <tummieofgaruda@gmail.com>
2019-10-10 10:01:33 -0700

    Mapitoto missing dialog
    Mapitoto didn't say anything when triggering or trading

--- scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
+++ scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
@@ -16,23 +16,29 @@ local ID = require("scripts/zones/Upper_Jeuno/IDs");
 
 function onTrade(player,npc,trade)
     if (trade:hasItemQty(10049,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(RAPTOR_COMPANION);
+	    player:addKeyItem(dsp.ki.RAPTOR_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.RAPTOR_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Raptor Mount!!", 0xD);
 	elseif (trade:hasItemQty(10051,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(CRAB_COMPANION);
+	    player:addKeyItem(dsp.ki.CRAB_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.CRAB_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Cab Mount!!", 0xD);
 	elseif (trade:hasItemQty(10050,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(TIGER_COMPANION);
+	    player:addKeyItem(dsp.ki.TIGER_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.TIGER_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Tiger Mount!!", 0xD);
 	elseif (trade:hasItemQty(10053,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(BOMB_COMPANION);
+	    player:addKeyItem(dsp.ki.BOMB_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.BOMB_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Bomb Mount!!", 0xD);
 	elseif (trade:hasItemQty(10056,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(CRAWLER_COMPANION);
+	    player:addKeyItem(dsp.ki.CRAWLER_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.CRAWLER_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Crawler Mount!!", 0xD);
 	elseif (trade:hasItemQty(10058,1) and player:hasKeyItem(dsp.ki.CHOCOBO_LICENSE) and player:getMainLvl() >= 20) then
-	    player:addKeyItem(BEETLE_COMPANION);
+	    player:addKeyItem(dsp.ki.BEETLE_COMPANION);
 		player:messageSpecial(ID.text.KEYITEM_OBTAINED,dsp.ki.BEETLE_COMPANION);
+        player:PrintToPlayer("Mapitoto : Enjoy your Beetle Mount!!", 0xD);
     end
 end;
 
@@ -41,10 +47,11 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-
-
-
-
+    if (player:hasKeyitem(dsp.ki.CHOCOBO_LICENSE)) then
+        player:PrintToPlayer("Mapitoto : If you have any of those companion slips, trade them to me and you will be able to get a mount!", 0xD);
+    else
+        player:PrintToPlayer("Mapitoto : Kweh!!!", 0xD);
+    end
 end;
 
 -----------------------------------
