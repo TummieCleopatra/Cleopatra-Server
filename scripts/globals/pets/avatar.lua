1fe930d218e1ffe62d1050b681679c1c06c49db2
Tummie <tummieofgaruda@gmail.com>
2019-11-11 12:20:59 -0800

    Avatar 2h despawn for Adv Summon
    Addin desummon method after Adv Summons use 2Hr

--- scripts/globals/pets/avatar.lua
+++ scripts/globals/pets/avatar.lua
@@ -9,8 +9,9 @@ require("scripts/globals/pets")
 function onMobSpawn(mob)
     local master = mob:getMaster()
     local petID = master:getPetID()
+    mob:setLocalVar("favorOn",0)
     mob:setLocalVar("afCool",2)
-    mob:setLocalVar("despawn",7)
+    mob:setLocalVar("despawn",os.time() + 9)
     mob:setLocalVar("afTime",os.time())
     printf("spawnnned")
 
@@ -33,13 +34,14 @@ function onMobSpawn(mob)
             local afCooldown = mob:getLocalVar("afCool")
             local afTime = mob:getLocalVar("afTime")
             local afDespawn = mob:getLocalVar("despawn")
-            print(battletime)
-            print(afcool)
             if (battletime > afTime + afCooldown) then
                 mob:useMobAbility(2334)
                 mob:setLocalVar("afTime",battletime)
-                mob:setLocalVar("despawn",battletime + 5)
+                mob:setLocalVar("afCool",20)
+            elseif (battletime >  afDespawn) then
+                mob:setHP(0)
             end
+
         end)
     end
 
@@ -48,9 +50,32 @@ function onMobSpawn(mob)
             local battletime = os.time()
             local afCooldown = mob:getLocalVar("afCool")
             local afTime = mob:getLocalVar("afTime")
+            local afDespawn = mob:getLocalVar("despawn")
             if (battletime > afTime + afCooldown) then
-                mob:useMobAbility(2370)
+                mob:useMobAbility(2370, mob)
                 mob:setLocalVar("afTime",battletime)
+                mob:setLocalVar("afCool",20)
+            elseif (battletime >  afDespawn) then
+                mob:setHP(0)
+            end
+        end)
+    end
+
+    if (petID <= 20) then
+        mob:addListener("COMBAT_TICK", "AVATAR_FAVOR", function(mob, player, target)
+            local favorOn = mob:getLocalVar("favorOn")
+            if (player:hasStatusEffect(dsp.effect.AVATAR_S_FAVOR) and favorOn ~= 1) then
+                local skill = player:getSkillLevel(dsp.skill.SUMMONING_MAGIC);
+                local attBuff = (skill / 250) + 3;
+                local accBuff = (skill / 10) + 1;
+                local mabBuff = (skill / 10) + 1;
+                local regain = (skill / 10) + 10;
+
+                mob:addMod(dsp.mod.ATTP, attBuff)
+                mob:addMod(dsp.mod.ACC, accBuff)
+                mob:addMod(dsp.mod.MAB, mabBuff)
+                mob:addMod(dsp.mod.REGAIN, regain)
+                mob:setLocalVar("favorOn",1)
             end
         end)
     end
