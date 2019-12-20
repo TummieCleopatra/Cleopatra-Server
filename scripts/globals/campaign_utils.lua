723d032dea12b89fd1c54135b40fc4d8d9b6438c
Tummie <tummieofgaruda@gmail.com>
2019-12-20 10:59:07 -0800

    Include campaign utils
    campaign utils file for later usage

---
+++ scripts/globals/campaign_utils.lua
@@ -0,0 +1,10 @@
+local ID = require("scripts/zones/West_Sarutabaruta_[S]/IDs")
+require("scripts/zones/West_Sarutabaruta_[S]/globals");
+require("scripts/globals/mobs")
+require("scripts/globals/pathfind");
+require("scripts/globals/status");
+
+-- Move NPC ID's to a different file...maybe here??
+function campaignMobAggro(mob)
+
+end;
