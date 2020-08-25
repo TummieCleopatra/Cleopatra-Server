3b23e69c393b546dbea33bd9cbc25b39578d3d97
Tummie <tummieofgaruda@gmail.com>
2020-08-25 14:05:13 -0700

    Oboro Chatoyant/Iridal Upgrade
    Chatoyant Upgrades for damage

--- scripts/zones/Port_Jeuno/npcs/Oboro.lua
+++ scripts/zones/Port_Jeuno/npcs/Oboro.lua
@@ -1139,6 +1139,71 @@ function onTrade(player,npc,trade)
   end;
 
   -----------------------------------
+  --      Chatoyant/Iridal         --
+  -----------------------------------
+
+  -- Iridal Staff (18632)
+  if (trade:hasItemQty(18632, 1) and trade:hasItemQty( 1126, 30 ) and level >= 51)
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18632, 1, 45, 4);
+    player:setVar("IridalUpgrade", 1);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18632);
+
+  elseif (trade:hasItemQty(18632, 1) and trade:hasItemQty( 1126, 50 ) and level >= 61 and (player:getVar("IridalUpgrade") == 1)) then
+    -- Complete the trade..
+    player:tradeComplete();
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18632, 1, 45, 9);
+    player:setVar("IridalUpgrade", 2);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18632);
+
+  elseif (trade:hasItemQty(18632, 1) and trade:hasItemQty( 1127, 50 ) and level >= 71 and (player:getVar("IridalUpgrade") == 2)) then
+    -- Complete the trade..
+    player:tradeComplete();
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18632, 1, 45, 14, 353, 0);
+    player:setVar("IridalUpgrade", 3);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18632);
+  elseif (trade:hasItemQty(18632, 1) and player:getVar("IridalUpgrade") < 1) then
+    player:PrintToPlayer("Oboro : I can upgrade your Idiral Staff if you bring me 30 Beastmen Seals.", 0xD);
+  elseif (trade:hasItemQty(18632, 1) and player:getVar("IridalUpgrade") == 1) then
+    player:PrintToPlayer("Oboro : I can upgrade your Iridal Staff if you bring me 50 Beastmen Seals.", 0xD);
+  elseif (trade:hasItemQty(18632, 1) and player:getVar("IridalUpgrade") == 2) then
+    player:PrintToPlayer("Oboro : I can upgrade your Iridal Staff if you bring me 50 Kindred Seals.", 0xD);
+  end
+
+
+  -- Chatoyant Staff (18633)
+  if (trade:hasItemQty(18633, 1) and trade:hasItemQty( 1126, 30 ) and level >= 51)
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18633, 1, 45, 4);
+    player:setVar("ChatoyantUpgrade", 1);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18633);
+
+  elseif (trade:hasItemQty(18633, 1) and trade:hasItemQty( 1126, 50 ) and level >= 61 and (player:getVar("ChatoyantUpgrade") == 1)) then
+    -- Complete the trade..
+    player:tradeComplete();
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18633, 1, 45, 9);
+    player:setVar("ChatoyantUpgrade", 2);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18633);
+
+  elseif (trade:hasItemQty(18633, 1) and trade:hasItemQty( 1127, 50 ) and level >= 71 and (player:getVar("ChatoyantUpgrade") == 2)) then
+    -- Complete the trade..
+    player:tradeComplete();
+    player:PrintToPlayer("Oboro : Thank you for the requested items. Come see me again when you are stronger.", 0xD);
+    player:addItem(18633, 1, 45, 14, 353, 0);
+    player:setVar("ChatoyantUpgrade", 3);
+    player:messageSpecial(ID.text.ITEM_OBTAINED, 18633);
+  elseif (trade:hasItemQty(18633, 1) and player:getVar("ChatoyantUpgrade") < 1) then
+    player:PrintToPlayer("Oboro : I can upgrade your Chatoyant if you bring me 30 Beastmen Seals.", 0xD);
+  elseif (trade:hasItemQty(18633, 1) and player:getVar("ChatoyantUpgrade") == 1) then
+    player:PrintToPlayer("Oboro : I can upgrade your Chatoyant if you bring me 50 Beastmen Seals.", 0xD);
+  elseif (trade:hasItemQty(18633, 1) and player:getVar("ChatoyantUpgrade") == 2) then
+    player:PrintToPlayer("Oboro : I can upgrade your Chatoyant if you bring me 50 Kindred Seals.", 0xD);
+  end
+
+  -----------------------------------
   -- onTrigger Action
   -----------------------------------
 
