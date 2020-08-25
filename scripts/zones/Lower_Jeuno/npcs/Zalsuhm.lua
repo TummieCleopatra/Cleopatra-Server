37ffb6aaa7acd916577d6b0057748c5cf91aeee3
Tummie <tummieofgaruda@gmail.com>
2020-08-25 11:35:12 -0700

    Mythic WS Quest Bugfix
    Fixes to Mythic WS quest

--- scripts/zones/Lower_Jeuno/npcs/Zalsuhm.lua
+++ scripts/zones/Lower_Jeuno/npcs/Zalsuhm.lua
@@ -9,6 +9,7 @@ require("scripts/globals/npc_util")
 require("scripts/globals/quests")
 require("scripts/globals/status")
 require("scripts/globals/weaponskillids")
+require("scripts/globals/keyitems")
 -----------------------------------
 
 -----------------------------------
@@ -67,10 +68,10 @@ function onTrigger(player,npc)
 	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Mystic Boon'? Bring me back a Maroon Seal", 0xD);
 		player:addQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_WHITE_MAGE);
 	elseif (quest == 1 and job == 4 and level >= 75 and blmquest == QUEST_AVAILABLE) then
-	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Vidounir? Bring me back a Cerise Seal", 0xD);
+	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Vidounir'? Bring me back a Cerise Seal", 0xD);
 		player:addQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_BLACK_MAGE);
 	elseif (quest == 1 and job == 5 and level >= 75 and rdmquest == QUEST_AVAILABLE) then
-	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Death Blossom? Bring me back a Cerise Seal", 0xD);
+	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Death Blossom'? Bring me back a Cerise Seal", 0xD);
 		player:addQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_RED_MAGE);
 	elseif (quest == 1 and job == 6 and level >= 75 and thfquest == QUEST_AVAILABLE) then
 	    player:PrintToPlayer("Zalsuhm : So you want to learn the secrets of 'Mandalic Stab'? Bring me back a Cerise Seal", 0xD);
@@ -175,70 +176,70 @@ function onTrigger(player,npc)
 	-- ------------------------------------ --
 	elseif (quest == 1 and job == 1 and level >= 75 and warquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("warquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,1);
 	elseif (quest == 1 and job == 2 and level >= 75 and mnkquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("mnkquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,2);
 	elseif (quest == 1 and job == 3 and level >= 75 and whmquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("whmquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,3);
 	elseif (quest == 1 and job == 4 and level >= 75 and blmquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("blmquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,4);
 	elseif (quest == 1 and job == 5 and level >= 75 and rdmquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("rdmquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,5);
 	elseif (quest == 1 and job == 6 and level >= 75 and thfquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("thfquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,6);
 	elseif (quest == 1 and job == 7 and level >= 75 and pldquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("pldquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,7);
 	elseif (quest == 1 and job == 8 and level >= 75 and drkquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("drkquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,8);
 	elseif (quest == 1 and job == 8 and level >= 75 and bstquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("bstquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,9);
 	elseif (quest == 1 and job == 10 and level >= 75 and brdquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("brdquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,10);
 	elseif (quest == 1 and job == 11 and level >= 75 and rngquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("rngquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,11);
 	elseif (quest == 1 and job == 12 and level >= 75 and samquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("samquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,12);
 	elseif (quest == 1 and job == 13 and level >= 75 and ninquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("ninquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,13);
 	elseif (quest == 1 and job == 14 and level >= 75 and drgquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("drgquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,14);
 	elseif (quest == 1 and job == 15 and level >= 75 and smnquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.CERISE_SEAL))) then
 	    player:setVar("smnquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,15);
 	elseif (quest == 1 and job == 16 and level >= 75 and bluquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("bluquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,16);
 	elseif (quest == 1 and job == 17 and level >= 75 and corquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("corquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,17);
 	elseif (quest == 1 and job == 18 and level >= 75 and pupquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.PINE_GREEN_SEAL))) then
 	    player:setVar("pupquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,18);
 	elseif (quest == 1 and job == 19 and level >= 75 and dncquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("dncquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,19);
 	elseif (quest == 1 and job == 20 and level >= 75 and schquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("schquestweapondone",1);
-		player:startEvent(0x2768);
-	elseif (quest == 1 and job == 21 and level >= 75 and schquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
+		player:startEvent(0x2768,20);
+	elseif (quest == 1 and job == 21 and level >= 75 and geoquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("geoquestweapondone",1);
-		player:startEvent(0x2768);
-	elseif (quest == 1 and job == 22 and level >= 75 and schquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
+		player:startEvent(0x2768,21);
+	elseif (quest == 1 and job == 22 and level >= 75 and runquest == QUEST_ACCEPTED and (player:hasKeyItem(dsp.ki.MAROON_SEAL))) then
 	    player:setVar("runquestweapondone",1);
-		player:startEvent(0x2768);
+		player:startEvent(0x2768,22);
 	end
 
 end;
@@ -258,7 +259,7 @@ end;
 
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
---printf("RESULT: %u",option);
+printf("RESULT: %u",option);
 
         local jobs =
         {
@@ -288,138 +289,141 @@ function onEventFinish(player,csid,option)
 
     local skill = jobs[option]
 
-	if (csid == 10088 and player:getVar("warquestweapondone",1)) then
+    print(skill)
+
+	if (csid == 10088 and player:getVar("warquestweapondone") == 1 and option == 1) then
 	    player:setVar("warquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_WARRIOR);
-		player:messageSpecial(ID.text.LEARN_WS,90);
+		player:PrintToPlayer("You have learned the weapon skill King's Justice!",0x15);
 		player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("mnkquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("mnkquestweapondone") == 1 and option == 2) then
 	    player:setVar("mnkquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_MONK);
-		player:messageSpecial(ID.text.LEARN_WS,11);
+		player:PrintToPlayer("You have learned the weapon skill Ascetic's Fury!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("whmquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("whmquestweapondone") == 1 and option == 3) then
 	    player:setVar("whmquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_WHITE_MAGE);
-		player:messageSpecial(ID.text.LEARN_WS,171);
+		player:PrintToPlayer("You have learned the weapon skill Mystic Boon!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("blmquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("blmquestweapondone") == 1 and option == 4) then
 	    player:setVar("blmquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_BLACK_MAGE);
-		player:messageSpecial(ID.text.LEARN_WS,186);
+		player:PrintToPlayer("You have learned the weapon skill Vidohunir!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("rdmquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("rdmquestweapondone") == 1 and option == 5) then
 	    player:setVar("rdmquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_RED_MAGE);
-		player:messageSpecial(ID.text.LEARN_WS,44);
-        player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("thfquestweapondone",1)) then
+		player:PrintToPlayer("You have learned the weapon skill Death Blossom!",0x15);
+        printf("ADD WEAPONSKILL %u",skill)
+        player:addLearnedWeaponskill(20)
+	elseif (csid == 10088 and player:getVar("thfquestweapondone") == 1 and option == 6) then
 	    player:setVar("thfquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_THIEF);
-		player:messageSpecial(ID.text.LEARN_WS,27);
+		player:PrintToPlayer("You have learned the weapon skill Mandalic Stab!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("pldquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("pldquestweapondone") == 1 and option == 7) then
 	    player:setVar("pldquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_PALADIN);
-		player:messageSpecial(ID.text.LEARN_WS,45);
+		player:PrintToPlayer("You have learned the weapon skill Atonement!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("drkquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("drkquestweapondone") == 1 and option == 8) then
 	    player:setVar("drkquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_DARK_KNIGHT);
-		player:messageSpecial(ID.text.LEARN_WS,106);
+		player:PrintToPlayer("You have learned the weapon skill Insurgency!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("bstquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("bstquestweapondone") == 1 and option == 9) then
 	    player:setVar("bstquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_BEASTMASTER);
-		player:messageSpecial(ID.text.LEARN_WS,74);
+		player:PrintToPlayer("You have learned the weapon skill Primal Rend!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("brdquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("brdquestweapondone") == 1 and option == 10) then
 	    player:setVar("brdquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_BARD);
-		player:messageSpecial(ID.text.LEARN_WS,28);
+		player:PrintToPlayer("You have learned the weapon skill Mordant Rime!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("rngquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("rngquestweapondone") == 1 and option == 11) then
 	    player:setVar("rngquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_RANGER);
-		player:messageSpecial(ID.text.LEARN_WS,217);
+		player:PrintToPlayer("You have learned the weapon skill Trueflight!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("samquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("samquestweapondone") == 1 and option == 12) then
 	    player:setVar("samquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_SAMURAI);
-		player:messageSpecial(ID.text.LEARN_WS,154);
+		player:PrintToPlayer("You have learned the weapon skill Tachi: Rana!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("ninquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("ninquestweapondone") == 1 and option == 13) then
 	    player:setVar("ninquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_NINJA);
-		player:messageSpecial(ID.text.LEARN_WS,138);
+		player:PrintToPlayer("You have learned the weapon skill Blade: Kamu!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("drgquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("drgquestweapondone") == 1 and option == 14) then
 	    player:setVar("drgquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_DRAGOON);
-		player:messageSpecial(ID.text.LEARN_WS,122);
+		player:PrintToPlayer("You have learned the weapon skill Drakesbane!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("smnquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("smnquestweapondone") == 1 and option == 15) then
 	    player:setVar("smnquestweapondone",0);
 		player:delKeyItem(dsp.ki.CERISE_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_SUMMONER);
-		player:messageSpecial(ID.text.LEARN_WS,187);
+		player:PrintToPlayer("You have learned the weapon skill Garland of Bliss!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("bluquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("bluquestweapondone") == 1 and option == 16) then
 	    player:setVar("bluquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_BLUE_MAGE);
-		player:messageSpecial(ID.text.LEARN_WS,46);
+		player:PrintToPlayer("You have learned the weapon skill Expaciation!",0x15);
         player:addLearnedWeaponskill(22);
-	elseif (csid == 10088 and player:setVar("corquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("corquestweapondone") == 1 and option == 17) then
 	    player:setVar("corquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_CORSAIR);
-		player:messageSpecial(ID.text.LEARN_WS,218);
+		player:PrintToPlayer("You have learned the weapon skill Leaden Salute!",0x15);
 		player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("pupquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("pupquestweapondone") == 1 and option == 18) then
 	    player:setVar("pupquestweapondone",0);
 		player:delKeyItem(dsp.ki.PINE_GREEN_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_PUPPETMASTER);
-		player:messageSpecial(ID.text.LEARN_WS,12);
+		player:PrintToPlayer("You have learned the weapon skill Stringing Pummel!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("dncquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("dncquestweapondone") == 1 and option == 19) then
 	    player:setVar("dncquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_DANCER);
-		player:messageSpecial(ID.text.LEARN_WS,29);
+		player:PrintToPlayer("You have learned the weapon skill Pyrrhic Kleos!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("schquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("schquestweapondone") == 1 and option == 20) then
 	    player:setVar("schquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
-		player:completeQuest(JEUNO,dsp.quest.id.jeuno.	UNLOCKING_A_MYTH_SCHOLAR);
-		player:messageSpecial(ID.text.LEARN_WS,188);
+		player:completeQuest(JEUNO,dsp.quest.id.jeuno.UNLOCKING_A_MYTH_SCHOLAR);
+		player:PrintToPlayer("You have learned the weapon skill Omniscience!",0x15);
         player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("geoquestweapondone",1)) then
+	elseif (csid == 10088 and player:getVar("geoquestweapondone") == 1 and option == 0) then
 	    player:setVar("geoquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(ADOULIN,dsp.quest.id.adoulin.GEOMANCERRIFIC);
-		player:messageSpecial(ID.text.LEARN_WS,175);
-        player:addLearnedWeaponskill(skill)
-	elseif (csid == 10088 and player:setVar("runquestweapondone",1)) then
+		player:PrintToPlayer("You have learned the weapon skill Exudiation!",0x15);
+        player:addLearnedWeaponskill(49)
+	elseif (csid == 10088 and player:getVar("runquestweapondone") == 1 and option == 0) then
 	    player:setVar("runquestweapondone",0);
 		player:delKeyItem(dsp.ki.MAROON_SEAL);
 		player:completeQuest(ADOULIN,dsp.quest.id.adoulin.RUNE_FENCING_THE_NIGHT_AWAY);
-		player:messageSpecial(ID.text.LEARN_WS,61);
-        player:addLearnedWeaponskill(skill)
+		player:PrintToPlayer("You have learned the weapon skill Dimidiation!",0x15);
+        player:addLearnedWeaponskill(50)
 	end
 end;
 
